991d166f4ff7589a68053fc880eb3b88510c6f71
rtritto <ariele.1994@gmail.com>
2024-09-26 00:01:28 +0200

    Init

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,51 @@
 import vikeSolid from 'vike-solid/vite'
 import { vikeNode } from 'vike-node/plugin'
-import type { UserConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 import { resolve } from 'node:path'
 
-export default {
-  cacheDir: '.vite',
-  plugins: [
-    vikeNode('./src/server/index.ts'),
-    vikeSolid()
-  ],
-  server: {
-    port: 3000
-  },
-  preview: {
-    port: 3000
-  },
-  build: {
-    target: 'esnext',
-    outDir: '.vite/dist'
-  },
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer
-      ]
-    }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(import.meta.dirname, 'src')
+import getGlobalConfig from './config.default'
+import getMongo from '@/middlewares/db'
+
+
+export default defineConfig(async ({ mode }) => {
+  // Add ME_CONFIG_ env vars to process.env
+  Object.assign(process.env, loadEnv(mode, process.cwd(), 'ME_CONFIG_'))
+
+  global.config = getGlobalConfig()
+  global.mongo = getMongo()
+  await global.mongo.connect(global.config)
+
+  return {
+    cacheDir: '.vite',
+    plugins: [
+      vikeNode('./src/server/index.ts'),
+      vikeSolid()
+    ],
+    server: {
+      port: 3000
+    },
+    preview: {
+      port: 3000
+    },
+    envPrefix: 'ME_CONFIG_',
+    build: {
+      target: 'esnext',
+      outDir: '.vite/dist'
+    },
+    css: {
+      postcss: {
+        plugins: [
+          tailwindcss,
+          autoprefixer
+        ]
+      }
+    },
+    resolve: {
+      alias: {
+        '@': resolve(import.meta.dirname, 'src')
+      }
     }
   }
-} satisfies UserConfig
+})
