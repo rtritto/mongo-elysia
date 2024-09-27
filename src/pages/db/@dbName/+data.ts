dfbe6d7c0cbe075a4507a8bf8be142a6fd7c2761
rtritto <ariele.1994@gmail.com>
2024-09-27 18:43:30 +0200

    Add redirect to validation dbName and collectionName

--- src/pages/db/@dbName/+data.ts
+++ src/pages/db/@dbName/+data.ts
@@ -1,12 +1,18 @@
+import { redirect } from 'vike/abort'
 import type { DataSync } from 'vike/types'
 
 import { isValidDatabaseName } from '@/utils/validations'
 
 export const data: DataSync<DataDatabase> = (pageContext) => {
   const { dbName } = pageContext.routeParams
-  const validationRes = isValidDatabaseName(dbName)
-  if ('error' in validationRes) {
-    throw new Error(validationRes.error)
+  const validationDbRes = isValidDatabaseName(dbName)
+  if ('error' in validationDbRes) {
+    console.warn(validationDbRes.error)
+    throw redirect('/')
+  }
+  if (global.mongo.databases.includes(dbName) === false) {
+    console.warn(`Missing Database ${dbName}`)
+    throw redirect('/')
   }
   return {
     databases: global.mongo.databases,
--- src/pages/db/@dbName/@collectionName/+data.ts
+++ src/pages/db/@dbName/@collectionName/+data.ts
@@ -1,20 +1,33 @@
+import { redirect } from 'vike/abort'
 import type { DataAsync } from 'vike/types'
 
+import { EP_DB } from '@/configs/endpoints'
 import { isValidCollectionName, isValidDatabaseName } from '@/utils/validations'
 
 export const data: DataAsync<DataCollection> = async (pageContext) => {
   const { dbName, collectionName } = pageContext.routeParams
   const validationDbRes = isValidDatabaseName(dbName)
   if ('error' in validationDbRes) {
-    throw new Error(validationDbRes.error)
+    console.warn(validationDbRes.error)
+    throw redirect('/')
+  }
+  if (global.mongo.databases.includes(dbName) === false) {
+    console.warn(`Missing Database ${dbName}`)
+    throw redirect('/')
   }
   const validationCollRes = isValidCollectionName(collectionName)
   if ('error' in validationCollRes) {
-    throw new Error(validationCollRes.error)
+    console.warn(validationCollRes.error)
+    throw redirect(`${EP_DB}/${dbName}`)
+  }
+  const collections = await global.mongo.getCollections(dbName)
+  if (collections.includes(collectionName) === false) {
+    console.warn(`Missing Collection ${collectionName}`)
+    throw redirect(`${EP_DB}/${dbName}`)
   }
   return {
     databases: global.mongo.databases,
-    collections: await global.mongo.getCollections(dbName),
+    collections,
     dbName,
     collectionName,
     title: `Collection: ${collectionName}`
