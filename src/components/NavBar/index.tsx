442bcee92aa4bde4d83c51356caee5f0e9741ba0
rtritto <ariele.1994@gmail.com>
2024-09-27 19:00:42 +0200

    Comment NavBarMobile

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -6,13 +6,13 @@ const NavBar: Component = () => {
   return (
     <header class="sticky top-0 z-50">
       <nav class="bg-gray-800 text-neutral-content shadow-lg">
-        <div class="hidden md:flex">
-          <NavBarDesktop />
-        </div>
+        {/* <div class="hidden md:flex"> */}
+        <NavBarDesktop />
+        {/* </div> */}
 
-        <div class="md:hidden">
-          {/* TODO <NavBarMobile /> */}Mobile
-        </div>
+        {/* TODO <div class="md:hidden">
+          <NavBarMobile />
+        </div> */}
       </nav>
     </header>
   )
