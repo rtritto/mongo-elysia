7d1175a763df39e00d36129692602cdf01c9af72
rtritto <ariele.1994@gmail.com>
2024-09-28 19:23:17 +0200

    Disable button animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,22 @@
 import type { Config } from 'tailwindcss'
 import daisyui from 'daisyui'
+import themes from 'daisyui/src/theming/themes'
 
 export default {
   content: [
     './src/**/*.{ts,tsx}'
   ],
   daisyui: {
-    themes: ['dark']
+    themes: [
+      {
+        dark: {
+          ...themes['dark'],
+          '.btn': {
+            animation: 0
+          }
+        }
+      }
+    ]
   },
   plugins: [daisyui]
 } satisfies Config
